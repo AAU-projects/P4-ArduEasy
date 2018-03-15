a1506bf3153229466613c8e3a835057b1d672441
Jonas Alsen <grann.alsen@gmail.com>
2018-03-15 13:06:38 +0100

    main() added

---
+++ ArduEasyMaven/src/main/java/Program.java
@@ -0,0 +1,17 @@
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class Program
+{
+    public static void main(String args[])
+    {
+        ANTLRInputStream inputStream = new ANTLRInputStream();
+        ArduEasyLexer lexer = new ArduEasyLexer(inputStream);
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        ArduEasyParser parser = new ArduEasyParser(tokenStream);
+
+        ParseTree cst = parser.r();
+    }
+}
+
