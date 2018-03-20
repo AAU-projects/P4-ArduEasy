d0ac05213424d4ae7dcdb5ae16e187f7ca8018eb
llundbo <llundbo@live.dk>
2018-03-20 12:46:50 +0100

    If-else-ASTnodes
    AST nodes for if, else if, else are now implemented.

---
+++ ArduEasyMaven/src/main/java/Nodes/ElseIfNode.java
@@ -0,0 +1,10 @@
+package Nodes;
+
+public class ElseIfNode extends IfNode
+{
+    @Override
+    public String toString()
+    {
+        return "else if";
+    }
+}
---
+++ ArduEasyMaven/src/main/java/Nodes/StatementsNode.java
@@ -0,0 +1,6 @@
+package Nodes;
+
+public class StatementsNode extends Node
+{
+
+}
