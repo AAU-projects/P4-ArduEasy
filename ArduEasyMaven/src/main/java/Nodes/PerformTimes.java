94203996c05ce52d0bc92db271c0e5a9db2f748d
llundbo <llundbo@live.dk>
2018-03-20 13:43:09 +0100

    Perform-ASTNode

---
+++ ArduEasyMaven/src/main/java/Nodes/PerformTimes.java
@@ -0,0 +1,14 @@
+package Nodes;
+import java.util.ArrayList;
+
+public class PerformTimes extends PerformNode
+{
+    public ExpressionNode value;
+    public ArrayList<StatementsNode> Body = new ArrayList<StatementsNode>();
+
+    @Override
+    public String toString()
+    {
+        return "perform times";
+    }
+}
