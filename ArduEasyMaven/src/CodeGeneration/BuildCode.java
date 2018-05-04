a9ce3b6466cbf66766deb79e1b7626457f9c35ea
Alechj <alec@hotmail.dk>
2018-05-04 14:02:01 +0200

    Continuation of Code Generation

--- ArduEasyMaven/src/CodeGeneration/BuildCode.java
+++ ArduEasyMaven/src/CodeGeneration/BuildCode.java
@@ -1,19 +1,15 @@
 package CodeGeneration;
 
 import AST.Nodes.*;
-import SymbolTable.SymbolTable;
 import visitor.Visitor;
-
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 public class BuildCode implements Visitor
 {
     private PrintWriter writer;
     private HashMap<String, String> pinMap = new HashMap<String, String>();
+    private HashMap<String, List<String>> arrayList = new HashMap<String, List<String>>();
     private String room;
     private int indent = 0; //Used to indent statements
     private StringBuilder strB = new StringBuilder();
@@ -49,40 +45,61 @@ public class BuildCode implements Visitor
         strB.append(input).append(System.getProperty("line.separator"));
     }
 
+    private String ReadPin(String pin)
+    {
+        return (pin.contains("A") ? "analogRead" : "digitalRead") + "(" + pin + ")";
+    }
+
+    private String ReadPin(Object node)
+    {
+        if(!(node instanceof ExpressionNode)) return null;
+
+        String identifier = (String)((ExpressionNode)node).Accept(this);
+        if(node instanceof IdentifierNode)
+            return pinMap.containsKey(identifier) ? ReadPin(pinMap.get(identifier)) : identifier;
+
+        return identifier;
+    }
+
     @Override
     public Object Visit(ParanNode node)
     {
-        return "(" + node.child.Accept(this) + ")";
+        return "(" + ReadPin(node.child) + ")";
     }
 
     @Override
     public Object Visit(AdditiveNode node)
     {
-        return node.LeftChild.Accept(this) + " + " + node.RightChild.Accept(this);
+        return ReadPin(node.LeftChild) + " + " + ReadPin(node.RightChild);
     }
 
     @Override
     public Object Visit(AnalogPinNode node)
     {
-        return null;
+        return ReadPin("A" + node.Value);
     }
 
     @Override
     public Object Visit(ArrayDeclarationNode node)
     {
-        String result = "int " + room + "_" + node.Identifier.Accept(this) + "[] = {";
+        String result = "int " + "house_" + room + "_" + node.Identifier.Accept(this) + "[] = {";
         int i = 1;
+
+        List<String> templist = new ArrayList<String>();
 
         for (IdentifierNode value : node.Values)
         {
             if(value.toString().equals(node.Identifier.Value))
                 continue;
 
-            result += pinMap.get(value.toString());
+            templist.add(pinMap.get("house_" + room + "_" + value.toString()));
+            result += pinMap.get("house_" + room + "_" + value.toString());
 
             if(i++ < node.Values.size() - 1)
                 result += ", ";
         }
+
+        arrayList.put("house_" + room + "_" + node.Identifier.Accept(this), templist);
 
         result += "};";
 
@@ -94,7 +111,31 @@ public class BuildCode implements Visitor
     @Override
     public Object Visit(AssignmentNode node)
     {
-        Addtextln(node.Identifier.Accept(this) + " = " + node.Value.Accept(this) + ";");
+        String identifier = (String)node.Identifier.Accept(this);
+        String realvalue = (String)node.Value.Accept(this);
+        String value = pinMap.containsKey(identifier) ? ReadPin(pinMap.get(identifier)) : realvalue;
+
+        if(pinMap.containsKey(identifier))
+        {
+            if(pinMap.get(identifier).contains("A"))
+                Addtextln("analogWrite("+ pinMap.get(identifier) + ", " + realvalue + ");");
+            else
+                Addtextln("digitalWrite("+ pinMap.get(identifier) + ", " + realvalue + ");");
+        }
+        else if(arrayList.containsKey(identifier))
+        {
+            String writemode = arrayList.get(identifier).get(0).contains("A") ? "analogWrite" : "digitalWrite";
+            String iterator = "i" + UUID.randomUUID().toString().replaceAll("-", "");
+            Addtextln("for (int " + iterator + " = 0; " + iterator + " < " + arrayList.get(identifier).size() + "; " + iterator + "++)");
+            Addtextln("{");
+            IncreaseIndent();
+            Addtextln(writemode + "(" + identifier.replaceAll("\\.", "_") + "[" + iterator + "], " + value + ");");
+            DeacreaseIndent();
+            Addtextln("}");
+        }
+
+        else
+            Addtextln(identifier + " = " + value + ";");
         return null;
     }
 
@@ -124,6 +165,7 @@ public class BuildCode implements Visitor
     @Override
     public Object Visit(DeclarationNode node)
     {
+        String identifier = (String)node.Identifier.Accept(this);
         String type;
         String value;
 
@@ -135,22 +177,22 @@ public class BuildCode implements Visitor
         if(node.Value instanceof TimeNode || node.Value instanceof DayNode || node.Value instanceof StringNode || node.Value instanceof MonthNode)
             value = "\""+ node.Value.toString() + "\"";
         else
-            value = (String)node.Value.Accept(this);
+            value = pinMap.containsKey(identifier) ? ReadPin(pinMap.get(identifier)) : (String)node.Value.Accept(this);
 
-        Addtextln(type + " " + node.Identifier.Accept(this) + " = " + value + ";");
+        Addtextln(type + " " + identifier + " = " + value + ";");
         return null;
     }
 
     @Override
     public Object Visit(DigitalPinNode node)
     {
-        return null;
+        return ReadPin(String.valueOf(node.Value));
     }
 
     @Override
     public Object Visit(DivisionNode node)
     {
-        return node.LeftChild.Accept(this) + " / " + node.RightChild.Accept(this);
+        return ReadPin(node.LeftChild) + " / " + ReadPin(node.RightChild);
     }
 
     @Override
@@ -193,7 +235,7 @@ public class BuildCode implements Visitor
     @Override
     public Object Visit(EqualsNode node)
     {
-        return node.Left.Accept(this) + " == " + node.Right.Accept(this);
+        return ReadPin(node.Left) + " == " + ReadPin(node.Right);
     }
 
     @Override
@@ -253,17 +295,22 @@ public class BuildCode implements Visitor
     @Override
     public Object Visit(GreaterOrEqualNode node)
     {
-        return node.Left.Accept(this) + " >= " + node.Right.Accept(this);
+        return ReadPin(node.Left) + " >= " + ReadPin(node.Right);
     }
 
     @Override
     public Object Visit(GreaterThanNode node)
     {
-        return node.Left.Accept(this) + " > " + node.Right.Accept(this);
+        return ReadPin(node.Left) + " > " + ReadPin(node.Right);
     }
 
     @Override
     public Object Visit(IdentifierNode node)
+    {
+        return node.Value;
+    }
+
+    public Object Visit(IdentifierNode node, boolean read)
     {
         return node.Value;
     }
@@ -306,25 +353,25 @@ public class BuildCode implements Visitor
     @Override
     public Object Visit(LessOrEqualNode node)
     {
-        return node.Left.Accept(this) + " <= " + node.Right.Accept(this);
+        return ReadPin(node.Left) + " <= " + ReadPin(node.Right);
     }
 
     @Override
     public Object Visit(LessThanNode node)
     {
-        return node.Left.Accept(this) + " < " + node.Right.Accept(this);
+        return ReadPin(node.Left) + " < " + ReadPin(node.Right);
     }
 
     @Override
     public Object Visit(LogicalAndNode node)
     {
-        return node.Left.Accept(this) + " && " + node.Right.Accept(this);
+        return ReadPin(node.Left) + " && " + ReadPin(node.Right);
     }
 
     @Override
     public Object Visit(LogicalOrNode node)
     {
-        return node.Left.Accept(this) + " || " + node.Right.Accept(this);
+        return ReadPin(node.Left) + " || " + ReadPin(node.Right);
     }
 
     @Override
@@ -336,19 +383,19 @@ public class BuildCode implements Visitor
     @Override
     public Object Visit(MultiplicationNode node)
     {
-        return node.LeftChild.Accept(this) + " * " + node.RightChild.Accept(this);
+        return ReadPin(node.LeftChild) + " * " + ReadPin(node.RightChild);
     }
 
     @Override
     public Object Visit(NegateNode node)
     {
-        return "!" + node.child.Accept(this);
+        return "!" + ReadPin(node.child);
     }
 
     @Override
     public Object Visit(NotEqualsNode node)
     {
-        return node.Left.Accept(this) + " != " + node.Right.Accept(this);
+        return ReadPin(node.Left) + " != " + ReadPin(node.Right);
     }
 
     @Override
@@ -366,8 +413,8 @@ public class BuildCode implements Visitor
     @Override
     public Object Visit(PerformTimes node)
     {
-        String identifier = UUID.randomUUID().toString();
-        Addtextln("for (int " + identifier + " = 0; " + identifier + " < " + node.value.Accept(this) + "; " + identifier + "++");
+        String identifier = "i" + UUID.randomUUID().toString().replaceAll("-", "");
+        Addtextln("for (int " + identifier + " = 0; " + identifier + " < " + node.value.Accept(this) + "; " + identifier + "++)");
         Addtextln("{");
         IncreaseIndent();
 
@@ -402,8 +449,9 @@ public class BuildCode implements Visitor
     @Override
     public Object Visit(PinDeclarationNode node)
     {
-        Addtextln("pinMode(" + node.Pin.Value + ", " + node.IoStatus.Value + ");");
-        pinMap.put(node.Identifier.toString(), String.valueOf(node.Pin.Value));
+        String pin = node.Pin instanceof AnalogPinNode ? "A" : "";
+        pin += node.Pin.Value;
+        Addtextln("pinMode(" + pin + ", " + node.IoStatus.Value.toUpperCase() + ");");
         return null;
     }
 
@@ -459,6 +507,36 @@ public class BuildCode implements Visitor
     @Override
     public Object Visit(SetupNode node)
     {
+
+        //We then add all the pin declerations
+        for (DefinitionNode child : node.Childs)
+        {
+            if(child instanceof PinDeclarationNode)
+            {
+                String identifier = ((PinDeclarationNode) child).Identifier.Value;
+                String pin = (((PinDeclarationNode) child).Pin instanceof AnalogPinNode) ? "A" : "";
+                pin += String.valueOf(((PinDeclarationNode) child).Pin.Value);
+                pinMap.put("house_" + identifier, pin);
+
+            }
+
+            if(child instanceof RoomDeclaration)
+            {
+                room = child.Identifier.toString();
+
+                for (Object grandchild : ((RoomDeclaration) child).body)
+                {
+                    if(grandchild instanceof PinDeclarationNode)
+                    {
+                        String identifier = ((PinDeclarationNode) grandchild).Identifier.Value;
+                        String pin = (((PinDeclarationNode) grandchild).Pin instanceof AnalogPinNode) ? "A" : "";
+                        pin += String.valueOf(((PinDeclarationNode) grandchild).Pin.Value);
+                        pinMap.put("house_" + room + "_" + identifier, pin);
+                    }
+                }
+            }
+        }
+
         //First we add the global variables,
         for (DefinitionNode child : node.Childs)
         {
@@ -506,7 +584,7 @@ public class BuildCode implements Visitor
     @Override
     public Object Visit(SubtractiveNode node)
     {
-        return node.LeftChild.Accept(this) + " - " + node.RightChild.Accept(this);
+        return ReadPin(node.LeftChild) + " - " + ReadPin(node.RightChild);
     }
 
     @Override
@@ -594,7 +672,7 @@ public class BuildCode implements Visitor
     @Override
     public Object Visit(HouseNode node)
     {
-        String[] split = node.Identifier.toString().split("\\.");
+        String[] split = ((String)node.Identifier.Accept(this)).split("\\.");
 
         if(split.length == 2)
             return split[0] + "_" + split[1];
