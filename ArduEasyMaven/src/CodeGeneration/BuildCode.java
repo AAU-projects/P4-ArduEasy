5928849c513c78ed73c5263e044ff4f0a2ec23d1
Alechj <alec@hotmail.dk>
2018-05-11 13:47:39 +0200

    Hotfix for declaration with a pin as value

--- ArduEasyMaven/src/CodeGeneration/BuildCode.java
+++ ArduEasyMaven/src/CodeGeneration/BuildCode.java
@@ -223,7 +223,8 @@ public class BuildCode implements Visitor
 
         if(node.Value instanceof MethodCallNode)
             MethodInlineCall = true;
-        value = pinMap.containsKey(identifier) ? ReadPin(pinMap.get(identifier)) : (String) node.Value.Accept(this);
+        String valueIdentifier = (String)node.Value.Accept(this);
+        value = pinMap.containsKey(valueIdentifier) ? ReadPin(pinMap.get(valueIdentifier)) : (String) node.Value.Accept(this);
 
         Addtextln(type + " " + identifier + " = " + value + (Inline ? "" : ";"));
         return null;
