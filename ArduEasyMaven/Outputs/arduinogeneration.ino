b0c069f0aa6e8b9550b5b270c0dfc1019495f4d2
HaZz <magnustboisen@gmail.com>
2018-06-20 09:32:19 +0200

    small test update

--- ArduEasyMaven/Outputs/arduinogeneration.ino
+++ ArduEasyMaven/Outputs/arduinogeneration.ino
@@ -1,16 +1,126 @@
+int Fff = 5;
+int FfF = 22;
+float bob2 = 5 + 8.5;
+int hej = 10 + 5;
+int hejhej = 2;
+String SavedTime = "00:00";
+String NextTime = "12:00";
+int house_Kitchen_Lamps[] = {1, 2};
+int house_Bedroom_Blinds[] = {6, 7, 8};
 
 void setup()
 {
-    pinMode(8, INPUT);
-    pinMode(13, OUTPUT);
+    pinMode(A12, INPUT);
+    pinMode(A13, INPUT);
+    pinMode(1, INPUT_PULLUP);
+    pinMode(2, INPUT_PULLUP);
+    pinMode(3, OUTPUT);
+    pinMode(4, INPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
 }
 
 void loop()
 {
-    if (digitalRead(8) == true)
+    if (digitalRead(4) == true)
     {
-        digitalWrite(13, !digitalRead(13));
+        if ((5 > 3) == true)
+        {
+        }
+        analogWrite(A12, 55);
+        CelciusToFahrenheit(house_Temp);        
+        float Fahrenheit = null;
+        digitalWrite(3, true);
+        bool test = SavedTime == NextTime;
+        if (SavedTime + "00:15" == NextTime && Fahrenheit >= 64 && test)
+        {
+            digitalWrite(5, true);
+        }
+    }
+    
+    if (SavedTime == "07:00")
+    {
+        for (int i4657eab598964fd787ee8d75ebc5b773 = 0; i4657eab598964fd787ee8d75ebc5b773 < 3; i4657eab598964fd787ee8d75ebc5b773++)
+        {
+            digitalWrite(house_Bedroom_Blinds[i4657eab598964fd787ee8d75ebc5b773], true);
+        }
+        for (int i72515fa883be4c3a934f6fb559eb8ab8 = 0; i72515fa883be4c3a934f6fb559eb8ab8 < 3; i72515fa883be4c3a934f6fb559eb8ab8++)
+        {
+            digitalWrite(house_Bedroom_Blinds[i72515fa883be4c3a934f6fb559eb8ab8], house_Kitchen_Lamps);
+        }
+        for (int idd4e7bc88da94b1ebd50cc040acfecc4 = 0; idd4e7bc88da94b1ebd50cc040acfecc4 < 2; idd4e7bc88da94b1ebd50cc040acfecc4++)
+        {
+            digitalWrite(house_Kitchen_Lamps[idd4e7bc88da94b1ebd50cc040acfecc4], false);
+        }
+    }
+    
+    if (SavedTime == "18:00")
+    {
+        for (int idc1845b7435f49d8ad3b4e3c2be2d8cc = 0; idc1845b7435f49d8ad3b4e3c2be2d8cc < 3; idc1845b7435f49d8ad3b4e3c2be2d8cc++)
+        {
+            digitalWrite(house_Bedroom_Blinds[idc1845b7435f49d8ad3b4e3c2be2d8cc], false);
+        }
+        int bob = 0;
+        int val = 5;
+        float bobjens = 50.25 * 32 * -1.4;
+        DetVirkerIkke(val);        
+        DetVirkerIkke(bob);        
+        CelciusToFahrenheit(bob);        
+        float bobbobbob = null;
+        if (val == 1)
+        {
+            bob = 88;
+        }
+        else if (val == 2)
+        {
+            if (true)
+            {
+            }
+        }
+        else if (val == 5)
+        {
+            bob = bob + val;
+        }
+        else
+        {
+            val = val + val;
+        }
     }
     
 }
 
+float CelciusToFahrenheit(int temp)
+{
+    float result = temp * 1.8 + 32;
+    String weekday = "MONDAY";
+    String test = "DECEMBER";
+    int anothertest = 89;
+    int bob = 4;
+    int i = 5;
+    for (i = 0;i < 10;i = i + 1)    
+    {
+        int jens = (5 + 5) * 8;
+    }
+    for (int j = 0;j < 10;j = j + 1)    
+    {
+        bool alec = true;
+        bool magnus = false;
+        bool party = (alec || magnus) && digitalRead(6);
+    }
+    for (int i284938541de94ecaa4fa295cef0b6c30 = 0; i284938541de94ecaa4fa295cef0b6c30 < 158; i284938541de94ecaa4fa295cef0b6c30++)
+    {
+    }
+    for (int i5215e8172bfb4359a0a7ec636f1b55a8 = 0; i5215e8172bfb4359a0a7ec636f1b55a8 < bob; i5215e8172bfb4359a0a7ec636f1b55a8++)
+    {
+    }
+    while (i > 15)
+    {
+    }
+    return result;
+}
+void DetVirkerIkke(int lol)
+{
+    int ajens = lol;
+}
--- ArduEasyMaven/Test/BulkTester.java
+++ ArduEasyMaven/Test/BulkTester.java
@@ -26,6 +26,7 @@ public class BulkTester
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_MAGENTA = "\u001B[35m";
 
     public boolean printErrors = false;
     private HashMap<String, String> testMap = new HashMap<String, String>();
@@ -43,20 +44,22 @@ public class BulkTester
 
         for (File folder : directories)
         {
-            System.out.println(ANSI_YELLOW + "Starting: " + folder.getName() + ANSI_RESET);
+            System.out.println(ANSI_MAGENTA + "Starting: " + folder.getName() + ANSI_RESET);
             File testDirectory = new File(folder.getPath());
             String[] testDirectories = testDirectory.list();
             for (String directory : testDirectories)
             {
                 System.out.println(ANSI_YELLOW + "Starting test: " + directory + ANSI_RESET);
-                Start(folder.getPath() + "\\" + directory);
+                int errors = Start(folder.getPath() + "\\" + directory);
                 testMap.put(directory, folder.getPath() + "\\" + directory);
-                System.out.println(ANSI_CYAN + "Successfully tested: " + directory + ANSI_RESET);
+                System.out.println(ANSI_YELLOW + "Successfully tested: " + directory + " with " + errors + " error(s)" + ANSI_RESET);
+                System.out.println(ANSI_CYAN + "------------------------------------------------ ");
             }
+            System.out.println(ANSI_MAGENTA + "Successfully tested: " + folder.getName() + ANSI_RESET);
         }
     }
 
-    public boolean Start(String testPath) throws  IOException
+    public int Start(String testPath) throws  IOException
     {
         CharStream inputStream = CharStreams.fromFileName(testPath);
         ArduEasyLexer lexer = new ArduEasyLexer(inputStream);
@@ -77,19 +80,19 @@ public class BulkTester
             typeChecker.Visit(root);
 
             ArrayList<Integer> expectedErrors = GetErrors(testPath);
-            CheckTest(expectedErrors);
+            int errors = CheckTest(expectedErrors);
 
 
             if (printErrors)
                 CustomErrorHandler.PrintErrors();
 
-            return true;
+            return errors;
 
         } catch (Exception e)
         {
             System.out.println(e.toString());
             e.printStackTrace();
-            return false;
+            return -1;
         }
         finally
         {
@@ -99,13 +102,15 @@ public class BulkTester
         }
     }
 
-    private void CheckTest(ArrayList<Integer> expectedErrors) {
+    private int CheckTest(ArrayList<Integer> expectedErrors) {
         List<Integer> errorsTypeChecker = CustomErrorHandler.GetLinenumberErrors();
+        int errors = 0;
 
         for (Integer errorTypeChecker : errorsTypeChecker) {
             if (!expectedErrors.contains(errorTypeChecker))
             {
                 System.out.println(ANSI_RED + "An error at line " + errorTypeChecker + " is NOT expected but one is found" + ANSI_RESET);
+                ++errors;
             }
         }
 
@@ -113,10 +118,12 @@ public class BulkTester
             if (!errorsTypeChecker.contains(expectedError))
             {
                 System.out.println(ANSI_RED + "An error at line " + expectedError + " is expected but one is NOT found" + ANSI_RESET);
+                ++errors;
             }
         }
 
-    }
+        return errors;
+   }
 
     private ArrayList<Integer> GetErrors(String fileName) throws FileNotFoundException, IOException {
         FileInputStream fstream = new FileInputStream(fileName);
